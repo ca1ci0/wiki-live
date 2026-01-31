88e69836ab0c0d3960b75bb74ed583d997bfbf6c
ca1ci0 <mykolakoval1997@gmail.com>
2026-01-31 19:23:31 +0000

    add stream-processor module

---
+++ stream-processor/src/main/java/io/wikilive/streamprocessor/WikimediaStreamProcessorApplication.java
@@ -0,0 +1,12 @@
+package io.wikilive.streamprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaStreamProcessorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WikimediaStreamProcessorApplication.class, args);
+  }
+}
